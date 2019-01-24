b9db66c1b4d0e0a4cead56a2464584aeb46d4906
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-01-24 20:42:20 +0100

    Updates

---
+++ precache-manifest.b2b8e2957af75af8081638b2a2971de4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b4ea524b34ab44580fc1",
+    "url": "/lesson-plan/static/css/main.39ae6ac8.chunk.css"
+  },
+  {
+    "revision": "b4ea524b34ab44580fc1",
+    "url": "/lesson-plan/static/js/main.b4ea524b.chunk.js"
+  },
+  {
+    "revision": "283eedd286b4028b79c7",
+    "url": "/lesson-plan/static/js/1.283eedd2.chunk.js"
+  },
+  {
+    "revision": "a7dd3ff3121f76e55274",
+    "url": "/lesson-plan/static/js/runtime~main.a7dd3ff3.js"
+  },
+  {
+    "revision": "1a5e02224fed7e515e8457bbfa4e250b",
+    "url": "/lesson-plan/index.html"
+  }
+];
