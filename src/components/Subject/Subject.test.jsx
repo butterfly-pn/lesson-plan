a9363bb800548d372f4b2eee06693d8500570007
Patryk Niedźwiedziński <pniedzwiedzinski19@gmail.com>
2019-01-25 14:05:52 +0100

    Remove enzyme

--- src/components/Subject/Subject.test.jsx
+++ src/components/Subject/Subject.test.jsx
@@ -1,13 +1,9 @@
 import React from "react";
-import { shallow } from "enzyme";
 import ReactDOM from "react-dom";
 import Subject from "./Subject";
 import renderer from "react-test-renderer";
 
 describe("<Subject />", () => {
-  const wrapper = shallow(
-    <Subject name="test" classroom="16" color="#000000" />
-  );
   it("renders without crashing", () => {
     const div = document.createElement("div");
     ReactDOM.render(
